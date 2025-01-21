d470ebf05232bc912fa9b0dd4fc2291d04e764bc
MatthewRobot <147113127+MatthewRobot@users.noreply.github.com>
2025-01-21 11:51:36 -0800

    initalized endgamePage
    Has a dropdown menu for endgame status and a gridview of buttons to keep track a preset notes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frc1148_2025_scouting_app/color_scheme.dart';
+import 'package:frc1148_2025_scouting_app/endgame.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,14 +23,15 @@ class _MainAppState extends State<MyApp> {
       theme: ThemeData.from(colorScheme: lightColorScheme),
       darkTheme: ThemeData.from(colorScheme: darkColorScheme),
       themeMode: themeMode,
-      home: MyHomePage(
-        onThemeChanged: (ThemeMode mode) {
-          setState(() {
-            themeMode = mode;
-          });
-        },
-        title: "meow",
-      ),
+      home: Endgame(teamName: "frc1148"),
+      // home: MyHomePage(
+      //   onThemeChanged: (ThemeMode mode) {
+      //     setState(() {
+      //       themeMode = mode;
+      //     });
+      //   },
+      //   title: "meow",
+      // ),
     );
   }
 }
