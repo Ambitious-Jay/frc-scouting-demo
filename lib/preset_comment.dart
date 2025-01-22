32b53f5872cd3b0dffe1a87fc4166fa9b69ce097
jbaca1-ship-it <jbaca1@hwemail.com>
2025-01-21 17:36:14 -0800

    better preset_comment ui
    just made things a bit prettier

--- lib/preset_comment.dart
+++ lib/preset_comment.dart
@@ -27,6 +27,7 @@ class _PresetCommentState extends State<PresetComment> {
     'strong': 1,
   };
 
+  // method to convert convert map into sorted list
   List makeList() {
     List commentsList = map.entries.toList();
     commentsList.sort((a, b) => b.value.compareTo(a.value));
@@ -35,6 +36,8 @@ class _PresetCommentState extends State<PresetComment> {
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     List commentsList = makeList();
 
     return Scaffold(
@@ -44,7 +47,14 @@ class _PresetCommentState extends State<PresetComment> {
         ),
         body: Column(
           children: [
-            const Text("Comments by frequency"),
+            Padding(
+              padding: EdgeInsets.all(height * 0.005),
+              child: Text(
+                "Comments by frequency",
+                style: TextStyle(fontSize: height * 0.0375),
+              ),
+            ),
+            const Divider(),
             Expanded(
               child: ListView.builder(
                 itemCount: commentsList.length,
@@ -53,16 +63,22 @@ class _PresetCommentState extends State<PresetComment> {
                   return Column(
                     children: [
                       Card(
+                          child: Padding(
+                        padding: EdgeInsets.all(width * 0.01),
                         child: Row(
                           children: [
-                            Text(entry.key),
-                            const VerticalDivider(
-                              color: Colors.white,
+                            Expanded(
+                              child: (Text(entry.key,
+                                  style: TextStyle(fontSize: width * 0.0625))),
                             ),
-                            Text(entry.value.toString())
+                            Text(
+                              entry.value.toString(),
+                              style: TextStyle(fontSize: width * 0.0625),
+                              textAlign: TextAlign.right,
+                            )
                           ],
                         ),
-                      ),
+                      )),
                       const Divider(
                         color: Colors.white,
                       )
