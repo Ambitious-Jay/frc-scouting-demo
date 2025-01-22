070051c9b5348295cec29c02bcaff32838335aca
Ambitious-Jay <andrewjo257@gmail.com>
2025-01-22 15:39:53 -0800

    semi flipping field

--- lib/auto_page.dart
+++ lib/auto_page.dart
@@ -18,12 +18,17 @@ class AutoPage extends StatefulWidget {
 
 class _AutoPageState extends State<AutoPage> {
   ThemeMode themeMode = ThemeMode.system;
+  bool isBlue = true;
   bool inCenterZone = false;
-  bool inProcessorZone = false;
-  bool inOppositeZone = false;
+  bool fieldFlipped = false;
+  bool inLeftZone = false;
+  bool inRightZone = false;
 
   double min(double valOne, double valTwo) {
     return valOne > valTwo ? valTwo : valOne;
+  }
+  double max(double valOne, double valTwo) {
+    return valOne > valTwo ? valOne : valTwo;
   }
 
   @override
@@ -33,8 +38,11 @@ class _AutoPageState extends State<AutoPage> {
     const double screenPadding = 12;
     const double imageWidthToHeight = 13 / 14;
     final double fieldWidth = min(screenWidth - 2 * screenPadding, 400);
+    final double fieldHeight = fieldWidth / imageWidthToHeight;
     // const AssetImage bg = AssetImage('assets/reefscape_blue_field.jpg');
-    const AssetImage bg = AssetImage('assets/reefscape_blue_field.jpg');
+    AssetImage bg = isBlue
+        ? const AssetImage('assets/reefscape_blue_field.jpg')
+        : const AssetImage('assets/reefscape_red_field.jpg');
     return Scaffold(
         appBar: AppBar(
           backgroundColor: colorScheme.primary,
@@ -46,6 +54,16 @@ class _AutoPageState extends State<AutoPage> {
               Text("${widget.id} is watching team ${widget.teamName}"),
             ],
           ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  fieldFlipped = !fieldFlipped;
+                });
+              },
+              icon: const Icon(Icons.rotate_90_degrees_ccw),
+            ),
+          ],
         ),
         body: Container(
             padding: const EdgeInsets.all(screenPadding),
@@ -60,33 +78,107 @@ class _AutoPageState extends State<AutoPage> {
                       child: Stack(alignment: Alignment.center, children: [
                         SizedBox(
                             width: fieldWidth,
-                            height: fieldWidth / imageWidthToHeight,
-                            child: const DecoratedBox(
+                            height: fieldHeight,
+                            child: Transform.rotate(
+                              angle: fieldFlipped ? 3.14159265 : 0,
+                              child: DecoratedBox(
                               decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                      image: bg, fit: BoxFit.fitWidth)),
-                            )),
-                        Checkbox(
-                            value: inCenterZone,
-                            onChanged: (bool? value) => {
-                                  setState(() {
-                                    inCenterZone = value!;
-                                  })
-                                }),
-                        Checkbox(
-                            value: inProcessorZone,
-                            onChanged: (bool? value) => {
-                                  setState(() {
-                                    inProcessorZone = value!;
-                                  })
-                                }),
-                        Checkbox(
-                            value: inOppositeZone,
-                            onChanged: (bool? value) => {
-                                  setState(() {
-                                    inOppositeZone = value!;
-                                  })
-                                }),
+                                image: DecorationImage(
+                                  image: bg, fit: BoxFit.fitWidth)),
+                              )
+                            ),
+                        ),
+                        Positioned(
+                            top: fieldHeight / 2 - 25,
+                            right: (fieldFlipped ? fieldWidth * 4 / 5 : fieldWidth / 4) - 25,
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  const Text("Center"),
+                                  Checkbox(
+                                      value: inCenterZone,
+                                      onChanged: (bool? value) => {
+                                            setState(() {
+                                              inCenterZone = value!;
+                                            })
+                                          })
+                                ])),
+                        Positioned(
+                          left: fieldWidth / 4,
+                          top: fieldHeight / 4 - 25,
+                          // top: 0,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children:[
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  const Text("Left"),
+                                  Checkbox(
+                                    value: inLeftZone,
+                                    onChanged: (bool? value) => {
+                                      setState(() {
+                                        inLeftZone = value!;
+                                      })
+                                    }
+                                  )
+                                ]
+                              ),
+                              SizedBox(
+                                height: max(0, fieldHeight * 5 / 12 - 50)
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  const Text("Right"),
+                                  Checkbox(
+                                    value: inRightZone,
+                                    onChanged: (bool? value) => {
+                                      setState(() {
+                                        inRightZone = value!;
+                                      })
+                                    }
+                                  )
+                                ]
+                              ),
+                            ]
+                          )
+                        ),
+                        // Positioned(
+                        //     top: fieldHeight / 4 - 36,
+                        //     left: fieldWidth / 4,
+                        //     child: Column(
+                        //         mainAxisAlignment: MainAxisAlignment.end,
+                        //         crossAxisAlignment: CrossAxisAlignment.center,
+                        //         children: [
+                        //           const Text("Left"),
+                        //           Checkbox(
+                        //               value: inLeftZone,
+                        //               onChanged: (bool? value) => {
+                        //                     setState(() {
+                        //                       inLeftZone = value!;
+                        //                     })
+                        //                   })
+                        //         ])),
+                        // Positioned(
+                        //     top: 3 * fieldHeight / 4 - 36,
+                        //     left: fieldWidth / 4,
+                        //     child: Column(
+                        //         mainAxisAlignment: MainAxisAlignment.end,
+                        //         crossAxisAlignment: CrossAxisAlignment.center,
+                        //         children: [
+                        //           const Text("Right"),
+                        //           Checkbox(
+                        //               value: inRightZone,
+                        //               onChanged: (bool? value) => {
+                        //                     setState(() {
+                        //                       inRightZone = value!;
+                        //                     })
+                        //                   })
+                        //         ])),
                       ])),
                   const SizedBox(
                       width: 50,
