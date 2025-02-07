7325648f37c24e336bf7e769fb60289dfef209c1
MatthewRobot <147113127+MatthewRobot@users.noreply.github.com>
2025-02-06 16:24:08 -0800

    added matchlist!
    doesn't have any backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:frc1148_2025_scouting_app/color_scheme.dart';
 import 'package:frc1148_2025_scouting_app/lead_scouting_page.dart';
+import 'package:frc1148_2025_scouting_app/match_list.dart';
 import 'package:frc1148_2025_scouting_app/team_stats_list.dart';
 import 'package:frc1148_2025_scouting_app/preset_comment.dart';
 
@@ -24,7 +25,8 @@ class _MainAppState extends State<MyApp> {
       darkTheme: ThemeData.from(colorScheme: darkColorScheme),
       themeMode: themeMode,
       // THE WAY MATTHEW HAD IT
-      home: LeadScoutingPage(teamName: "meow1 meow2 meow3"),
+      // home: LeadScoutingPage(teamName: "meow1 meow2 meow3"),
+      home: MatchList(),
       //  home: TeamStatsList(teamName: "meow"),
       // THE WAY JOSEPH HAD IT
       // home: PresetComment(
---
+++ lib/match_list.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+import 'color_scheme.dart';
+
+class MatchList extends StatefulWidget {
+  const MatchList({Key? key}) : super(key: key);
+  @override
+  State<MatchList> createState() => _MatchList();
+}
+
+class _MatchList extends State<MatchList> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  List<String> matches = List.empty(growable: true);
+
+  Future<void> _fillMatches(col) async {
+    // This gets the matches they we list
+    // try {
+    //   final sheet = await SheetsHelper.sheetSetup('TBA-data');
+
+    //   final cell = await sheet?.cells.column(col);
+    //   for (int i = 0; i < cell!.length; i++){
+    //     matches.add(cell[i].value);
+    //   }
+    //   setState(() {});
+
+    // } catch (e) {
+    //   print('Error: $e');
+    // }
+    matches.add("qm1");
+    matches.add("qm2");
+    matches.add("qm3");
+    matches.add("qm4");
+    matches.add("qm5");
+    matches.add("qm6");
+    matches.add("qm7");
+    matches.add("qm8");
+    matches.add("qm9");
+    matches.add("qm10");
+    matches.add("qm11");
+    matches.add("qm12");
+    matches.add("qm13");
+    matches.add("qm14");
+    matches.add("qm15");
+    matches.add("qm16");
+    matches.add("qm17");
+    matches.add("qm18");
+    matches.add("qm19");
+    matches.add("qm11");
+    matches.add("qm12");
+    matches.add("qm13");
+    matches.add("qm14");
+    matches.add("qm15");
+    matches.add("qm16");
+    matches.add("qm17");
+    matches.add("qm18");
+    matches.add("qm19");
+    setState(() {});
+  }
+
+// This fetches the contents of what will be displayed from buttons
+
+  // Future<List<String>> _fetchRow(String matchID) async {
+  //   try {
+
+  //   } catch (e) {
+  //     print('Error: $e');
+  //     return List.empty();
+  //   }
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: ListView.separated(
+        padding: const EdgeInsets.all(8),
+        itemCount: matches.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.white),
+            ),
+            child: SizedBox(
+              height: height / 12,
+              width: width / 20,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      side: BorderSide(color: Colors.grey, width: 1),
+                    ),
+                    child: Text(matches[index]),
+                    onPressed: () async {
+                      // This fetches what will be displayed (make conditional??)
+                      // List<String> matchData = await _fetchRow(matches[index]);
+                      // This navigates to the next page
+                      // Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute( builder: (context) =>  MatchListDisplay (matchData: matchData, matchID: matches[index]) )
+                      // );
+                    }),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      side: BorderSide(color: Colors.grey, width: 1),
+                      backgroundColor: Colors.blue,
+                    ),
+                    child: Text("Blue"),
+                    onPressed: () async {
+                      // This fetches what will be displayed (make conditional??)
+                      // List<String> matchData = await _fetchRow(matches[index]);
+                      // This navigates to the next page
+                      // Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute( builder: (context) =>  MatchListDisplay (matchData: matchData, matchID: matches[index]) )
+                      // );
+                    }),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      side: BorderSide(color: Colors.grey, width: 1),
+                      backgroundColor: Colors.red,
+                    ),
+                    child: Text("Red"),
+                    onPressed: () async {
+                      // This fetches what will be displayed (make conditional??)
+                      // List<String> matchData = await _fetchRow(matches[index]);
+                      // This navigates to the next page
+                      // Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute( builder: (context) =>  MatchListDisplay (matchData: matchData, matchID: matches[index]) )
+                      // );
+                    })
+              ],
+            ),
+            ),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) => Container(
+          alignment: AlignmentDirectional.center,
+          height: height / 150,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.refresh),
+          onPressed: () async {
+            await _fillMatches(1);
+          }),
+    );
+  }
+}
