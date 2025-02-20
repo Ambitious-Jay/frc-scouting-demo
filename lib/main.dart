ae29608af388d3db6ecc67a188ef32ab9fdcd2b5
MatthewRobot <147113127+MatthewRobot@users.noreply.github.com>
2025-02-19 16:11:45 -0800

    LeadScoutNotesVisPage is now on main

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:frc1148_2025_scouting_app/color_scheme.dart';
 import 'package:frc1148_2025_scouting_app/scatter_plot.dart';
-
+import 'package:frc1148_2025_scouting_app/lead_scout_notes_vis_page.dart';
 import 'package:frc1148_2025_scouting_app/Backend/auth_service.dart';
 import 'package:frc1148_2025_scouting_app/Backend/websocket_service.dart';
 import 'package:frc1148_2025_scouting_app/color_scheme.dart';
