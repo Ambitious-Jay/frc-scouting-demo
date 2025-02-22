b92c81c2231a1aa7b50d5b59f5a693d04d0d0bc8
jbaca1-ship-it <jbaca1@hwemail.com>
2025-02-21 16:22:48 -0800

    update endgame
    added l2/l3 preset comments
    changed hang data to park data

--- lib/endgame.dart
+++ lib/endgame.dart
@@ -7,7 +7,8 @@ import 'package:frc1148_2025_scouting_app/dashboard_page.dart';
 // import 'teleop_form.dart' as tf;
 import 'color_scheme.dart';
 
-String Hang = "";
+// String Hang = "";
+bool park = false;
 
 Map<String, bool> presets = <String, bool>{
   'Part Broke': false,
@@ -17,7 +18,9 @@ Map<String, bool> presets = <String, bool>{
   'Tippy': false,
   'Accurate Coral': false,
   'Defensive': false,
-  'Net Algae': false
+  'Net Algae': false,
+  'L2': false,
+  'L3': false
 };
 
 List<String> keys = presets.keys.toList();
@@ -51,12 +54,18 @@ class _Endgame extends State<Endgame> {
 
   String id = "";
 
+  void updatePark() {
+    park = !park;
+  }
+
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        centerTitle: true,
         title: Column(
           children: [
             const Text("Endgame"),
@@ -68,33 +77,71 @@ class _Endgame extends State<Endgame> {
         child: ListView(
           children: [
             SizedBox(
-              height: height / 3.5,
-              child: Center(
-                child: DropdownButtonFormField<String>(
-                  value: "No Value Entered/Seen",
-                  onChanged: (String? value) {
-                    Hang = value!;
-                  },
-                  decoration: const InputDecoration(
-                    labelText: 'Hang',
-                    border: OutlineInputBorder(),
-                  ),
-                  items: [
-                    'Deep Cage',
-                    'Shallow Cage',
-                    'Failed Deep Cage',
-                    'Failed Shallow Cage',
-                    'Did Not Try',
-                    "No Value Entered/Seen"
-                  ].map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
+                height: height / 3.5,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Text(
+                      "Attempt to park?",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          park = !park;
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        // backgroundColor: park
+                        //     ? Theme.of(context).colorScheme.primary
+                        //     : Theme.of(context).colorScheme.secondary,
+                        // foregroundColor: park
+                        //     ? Theme.of(context).colorScheme.onPrimary
+                        //     : Theme.of(context).colorScheme.onSecondary,
+                        backgroundColor:
+                            Theme.of(context).colorScheme.onPrimary,
+                        minimumSize: const Size(100, 100),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                      child: park
+                          ? const Icon(
+                              // Icons.library_add_check,
+                              Icons.done,
+                              size: 44,
+                              color: Colors.white,
+                            )
+                          : const SizedBox.shrink(),
+                    ),
+                  ],
+                )
+                // child: Center(
+                //   child: DropdownButtonFormField<String>(
+                //     value: "No Value Entered/Seen",
+                //     onChanged: (String? value) {
+                //       Hang = value!;
+                //     },
+                //     decoration: const InputDecoration(
+                //       labelText: 'Hang',
+                //       border: OutlineInputBorder(),
+                //     ),
+                //     items: [
+                //       'Deep Cage',
+                //       'Shallow Cage',
+                //       'Failed Deep Cage',
+                //       'Failed Shallow Cage',
+                //       'Did Not Try',
+                //       "No Value Entered/Seen"
+                //     ].map<DropdownMenuItem<String>>((String value) {
+                //       return DropdownMenuItem<String>(
+                //         value: value,
+                //         child: Text(value),
+                //       );
+                //     }).toList(),
+                //   ),
+                // ),
                 ),
-              ),
-            ),
             const Divider(),
             SingleChildScrollView(
                 child: GridView.builder(
