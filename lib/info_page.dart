263a87cf4b970578295cddf1999089cef2108a6c
jbaca1-ship-it <jbaca1@hwemail.com>
2025-02-21 17:40:15 -0800

    improved buttons
    i dont know whats up with the app bar cause i didnt make this file so i left it but it is not that attractive atm

--- lib/info_page.dart
+++ lib/info_page.dart
@@ -8,9 +8,6 @@ import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:frc1148_2025_scouting_app/Backend/auth_service.dart';
 import 'package:frc1148_2025_scouting_app/Backend/websocket_service.dart';
 import 'login_page.dart';
-import 'package:frc1148_2025_scouting_app/Backend/auth_service.dart';
-import 'login_page.dart';
-import 'package:frc1148_2025_scouting_app/Backend/websocket_service.dart';
 import 'package:frc1148_2025_scouting_app/color_scheme.dart';
 
 class InfoPage extends StatelessWidget {
@@ -41,6 +38,8 @@ class InfoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     final colorScheme = Theme.of(context).colorScheme;
 
     return Scaffold(
@@ -56,11 +55,13 @@ class InfoPage extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-              width: double.infinity,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: width * 0.9,
+              height: height * 0.2,
               margin: const EdgeInsets.all(5),
               child: ElevatedButton(
                 onPressed: () {
@@ -74,22 +75,21 @@ class InfoPage extends StatelessWidget {
                 style: ElevatedButton.styleFrom(
                   backgroundColor: colorScheme.primary,
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8), // Decrease the rounding
+                    borderRadius: BorderRadius.circular(8),
                   ),
                 ),
                 child: const Text(
                   'Match List',
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 20,
+                    fontSize: 54,
                   ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            child: Container(
-              width: double.infinity,
+            Container(
+              width: width * 0.9,
+              height: height * 0.2,
               margin: const EdgeInsets.all(5),
               child: ElevatedButton(
                 onPressed: () {
@@ -103,20 +103,76 @@ class InfoPage extends StatelessWidget {
                 style: ElevatedButton.styleFrom(
                   backgroundColor: colorScheme.primary,
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8), // Decrease the rounding
+                    borderRadius: BorderRadius.circular(8),
                   ),
                 ),
                 child: const Text(
-                  'Team list',
+                  'Team List',
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 20,
+                    fontSize: 54,
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+            Container(
+              width: width * 0.9,
+              height: height * 0.2,
+              margin: const EdgeInsets.all(5),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => TeamSearchPage(),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: colorScheme.primary,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: const Text(
+                  'Graphing',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 54,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              width: width * 0.9,
+              height: height * 0.2,
+              margin: const EdgeInsets.all(5),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => TeamSearchPage(),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: colorScheme.primary,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: const Text(
+                  'Scatter Plot',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 54,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
